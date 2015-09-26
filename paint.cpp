b8663232c8e5483563c8f85b278a47daa593bb14
katiemacmillan <katherine.macmillan@mines.sdsmt.edu>
2015-09-25 19:41:28 -0600

    added program header

--- paint.cpp
+++ paint.cpp
@@ -1,4 +1,35 @@
-/* paint.cpp */
+/**********************************************************************
+                            paint
+**********************************************************************
+Authors: Christine Sorensen & Katherine MacMillan
+**********************************************************************
+paint uses a shape class with three derived classes to create and
+draw various shapes.
+
+By using the left mouse button to click on a shape icon, or color on
+the color palette, the corresponding shape or color can be chosen.
+A color chosen in this manner will set the shape's border color to 
+the chosen color. 
+
+A shape is then created by using the left mouse button to click on
+two different points in the draw window. This program uses the two
+coordinates to draw a rectangle, line or ellipses. 
+
+By right clicking on a color in the color palette a color can be chosen
+to be used on the interior of a filled shape. By right clicking and 
+holding on a drawn shape, the shape with the center closest to the 
+selected point will be selected for movement. Moving the mouse without 
+releasing the button will cause the shape to be dragged to a new 
+location on the screen. When the right mouse button is release the 
+shape will stay in that new location. 
+
+**********************************************************************
+                            Known Bugs
+**********************************************************************
+On occation when selecting a shape to be moved, the program will
+instead move the previously moved shape to the newly selected point,
+rather than selecting a new shape. 
+**********************************************************************/
 
 #include <GL/freeglut.h>
 #include "globals.h"
@@ -66,6 +97,4 @@ void initOpenGL( void )
 
     // how to handle mouse pressed movement
     glutMotionFunc( mousedrag );
-
-    //glutPostRedisplay();
 }
